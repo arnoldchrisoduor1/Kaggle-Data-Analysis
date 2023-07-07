5685fde5aec8cb66c705e25975aded8e126dc474
Arnold Chris <arnoldchrisoduor@gmail.com>
2023-07-08 01:16:56 +0300

    assigning

--- Indexing_Selecting_Assigning/assigning.py
+++ Indexing_Selecting_Assigning/assigning.py
@@ -2,10 +2,5 @@ import pandas as pd
 
 linked = pd.read_csv('C:\\Users\\arnol\\OneDrive\\Desktop\\Kaggle-Data-Analysis\\src\\linkdin_Job_data.csv')
 
-analysts = pd.DataFrame(linked.loc[linked.job == 'Data Analyst'])
-print(analysts)
-
-#selecting all the data analyst jobs from recruiters that have less than 500 linked in followers.
-
 rec = pd.DataFrame(linked.loc[(linked.no_of_application < 500) & (linked.job == 'Data Analyst')])
 print(rec)
